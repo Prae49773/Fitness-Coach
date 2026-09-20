08a9cb0394fc290def658d46fc8eca44dcc32e11
Prae49773 <49773@mws.ac.th>
2026-09-20 23:53:19 +0700

    removing ngrock

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,19 @@ import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
 
-
-dotenv.config({ path: path.resolve(process.cwd(), 'env.env') })
+dotenv.config({ path: path.resolve(process.cwd(), '.env') })
 dotenv.config()
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+  proxy: {
+    '/api': {
+      target:'http://localhost:5000',
+      changeOrigin: false, //
+    },
+  },
+},
 })
