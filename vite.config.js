b66b074a6bae323f92a0ed44523aacc5dc8514b5
Prae49773 <49773@mws.ac.th>
2026-09-20 22:49:10 +0700

    remove netlify vite plugin

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin'
+
 
 dotenv.config({ path: path.resolve(process.cwd(), 'env.env') })
 dotenv.config()
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), netlify()],
+  plugins: [react(), tailwindcss(),],
 })
